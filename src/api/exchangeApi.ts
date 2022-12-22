1834957e61188ee54ffabb3eaa7cb0e8518946aa
Roy Prins <r.prins@electricant.com>
2022-12-22 10:52:08 +0100

    Update API key

--- src/api/exchangeApi.ts
+++ src/api/exchangeApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "pTRfPWmYAtJODOVMQQ2d5sDfVkZdEFSK";
+const API_KEY = "gM6eojoc2Dw9wLvh1az5gXdN5DJzMmj7";
 const API_ROOT = "https://api.apilayer.com/exchangerates_data/";
 
 const exchangeApi = axios.create({
